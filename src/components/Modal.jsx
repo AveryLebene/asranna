143ffd2842448e32fd10037c8b3d2c9006a2e5d7
AveryLebene <114759302+AveryLebene@users.noreply.github.com>
2023-04-11 20:21:28 +0000

    see all added events

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,19 @@
-// import ics from "ics";
-// import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import { IoAddCircleOutline } from "react-icons/io5";
-import { FiDownload } from "react-icons/fi";
-import { useEffect, useState } from "react";
-import FileSaver from "file-saver";
-import { createEvents } from "ics";
+import { useEffect, useState, useRef } from "react";
 
+import { createEvents } from "ics";
+import FileSaver from "file-saver";
+
+import { v4 as uuidv4 } from "uuid";
 import moment from "moment";
+
+import { IoAddCircleOutline } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FiDownload } from "react-icons/fi";
+import Modal from "react-bootstrap/Modal";
+import Table from "react-bootstrap/Table";
+import Dropdown from "react-bootstrap/Dropdown";
+import Popover from "react-bootstrap/Popover";
+import Overlay from "react-bootstrap/Overlay";
 
 function MyModal(props) {
   const [start, setStart] = useState("");
@@ -17,8 +23,14 @@ function MyModal(props) {
   const [location, setLocation] = useState("");
   const [events, setEvents] = useState([]);
   const [showEvents, setShowEvents] = useState(false);
+  const [editEvent, setEditEvent] = useState(null);
+  const [showPop, setShowPop] = useState(false);
+  const [target, setTarget] = useState(null);
 
-  //   const timeString = "19740108T054400";
+  const popRef = useRef(null);
+  const formRef = useRef(null);
+
+  //   const time = "19740108T054400";
   //   const formattedTime = moment(timeString, "YYYYMMDDTHHmmss")
   //     .toArray()
   //     .slice(0, 3)
@@ -31,29 +43,43 @@ function MyModal(props) {
   //   const formattedTime = [date.get("year"), month, date.get("date")];
 
   //   console.log(formattedTime);
+  //   start: moment(String(start), "YYYYMMDDTHHmmss").toArray().slice(0, 3),
+  //   end: moment(String(end), "YYYYMMDDTHHmmss").toArray().slice(0, 3),
 
   const handleAddEvent = (e) => {
     let startDate = moment(start, "YYYYMMDDTHHmmss");
     let startMonth = startDate.get("month") + 1;
-    let startArray = [startDate.get("year"), startMonth, startDate.get("date")];
+    let startArray = [
+      startDate.get("year"),
+      startMonth,
+      startDate.get("date"),
+      startDate.get("hour"),
+      startDate.get("minute"),
+    ];
 
     let endDate = moment(end, "YYYYMMDDTHHmmss");
     let endMonth = endDate.get("month") + 1;
-    let endArray = [endDate.get("year"), endMonth, endDate.get("date")];
+    let endArray = [
+      endDate.get("year"),
+      endMonth,
+      endDate.get("date"),
+      endDate.get("hour"),
+      endDate.get("minute"),
+    ];
 
     e.preventDefault();
     const newEvent = {
+      id: uuidv4(),
       title,
-      //   start: moment(String(start), "YYYYMMDDTHHmmss").toArray().slice(0, 3),
-      //   end: moment(String(end), "YYYYMMDDTHHmmss").toArray().slice(0, 3),
+
       start: startArray,
       end: endArray,
       description,
       location,
     };
     setEvents([...events, newEvent]);
-    // console.log("events", events);
-    console.log("added");
+    console.log("events", events);
+    // console.log("added");
 
     setTitle("");
     setStart("");
@@ -65,6 +91,41 @@ function MyModal(props) {
   useEffect(() => {
     console.log(events);
   }, [events]);
+
+  const handleEditEvent = (e, startArray, endArray) => {
+    e.preventDefault();
+
+    // formRef.current.focus();
+
+    const updatedEvent = {
+      ...editEvent,
+      title,
+      start: startArray,
+      end: endArray,
+      description,
+      location,
+    };
+
+    const updatedEvents = events.map((event) => {
+      if (event.id === updatedEvent.id) {
+        return updatedEvent;
+      } else {
+        return event;
+      }
+    });
+    setEvents(updatedEvents);
+
+    // setEditEvent(null);
+
+    setEvents(updatedEvents);
+
+    setTitle("");
+    setStart("");
+    setEnd("");
+    setDescription("");
+    setLocation("");
+    console.log(setEvents);
+  };
 
   const handleDownload = () => {
     const { error, value } = createEvents(events);
@@ -80,6 +141,39 @@ function MyModal(props) {
     console.log("val", value);
   };
 
+  function formatDate(date) {
+    const options = { year: "numeric", month: "short", day: "numeric" };
+    const formattedDate = new Date(
+      date[0],
+      date[1] - 1,
+      date[2],
+      date[3],
+      date[4]
+    );
+    return new Intl.DateTimeFormat("en-US", options).format(formattedDate);
+  }
+
+  function formatTime(date) {
+    const formattedTime = new Date(
+      date[0],
+      date[1] - 1,
+      date[2],
+      date[3],
+      date[4]
+    );
+    const options = { hour: "numeric", minute: "numeric", hour12: true };
+    return new Intl.DateTimeFormat("en-US", options).format(formattedTime);
+  }
+
+  function handleEditClick() {
+    formRef.current.focus();
+  }
+
+  const deleteClick = (event) => {
+    setShowPop(!showPop);
+    setTarget(event.target);
+  };
+
   return (
     <Modal
       {...props}
@@ -91,108 +185,178 @@ function MyModal(props) {
         <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form className="new-event" onSubmit={handleAddEvent}>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            list="events"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Event Title"
-            required
-          />
-          <datalist id="events">
-            <option value="Birthday">Birthday</option>
-            <option value="Anniversary">Anniversary</option>
-            <option value="Wedding">Wedding</option>
-            <option value="Graduation">Graduation</option>
-            <option value="Meeting">Meeting</option>
-          </datalist>
+        <div ref={popRef}>
+          <form
+            className="new-event"
+            onSubmit={editEvent ? handleEditEvent : handleAddEvent}
+          >
+            <input
+              type="text"
+              id="title"
+              name="title"
+              list="events"
+              value={editEvent ? editEvent.title : title}
+              onChange={(e) => {
+                editEvent
+                  ? setEditEvent(e.target.value)
+                  : setTitle(e.target.value);
+              }}
+              placeholder="Event Title"
+              required
+              ref={formRef}
+            />
+            <datalist id="events">
+              <option value="Birthday">Birthday</option>
+              <option value="Anniversary">Anniversary</option>
+              <option value="Wedding">Wedding</option>
+              <option value="Graduation">Graduation</option>
+              <option value="Meeting">Meeting</option>
+            </datalist>
 
-          <div className="time">
-            <div className="">
-              <label htmlFor="start">Start Date and Time</label>
-              <input
-                type="datetime-local"
-                id="start"
-                name="start"
-                value={start}
-                onChange={(e) => setStart(e.target.value)}
-                placeholder="Start Time"
-                required
-              />
+            <div className="time">
+              <div className="">
+                <label htmlFor="start">Start Date and Time</label>
+                <input
+                  type="datetime-local"
+                  id="start"
+                  name="start"
+                  value={editEvent ? editEvent.start : start}
+                  onChange={(e) =>
+                    editEvent
+                      ? setEditEvent(e.target.value)
+                      : setStart(e.target.value)
+                  }
+                  placeholder="Start Time"
+                  required
+                />
+              </div>
+              <div>
+                <label htmlFor="start">End Date and Time</label>
+                <input
+                  type="datetime-local"
+                  id="end"
+                  name="end"
+                  value={editEvent ? editEvent.end : end}
+                  onChange={(e) => setEnd(e.target.value)}
+                  placeholder="End Date and Time"
+                  required
+                />
+              </div>
             </div>
-            <div>
-              <label htmlFor="start">End Date and Time</label>
-              <input
-                type="datetime-local"
-                id="end"
-                name="end"
-                value={end}
-                onChange={(e) => setEnd(e.target.value)}
-                placeholder="End Date and Time"
-                required
-              />
+            <input
+              type="text"
+              id="description"
+              name="description"
+              placeholder="Event Description"
+              value={editEvent ? editEvent.description : description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="text"
+              id="location"
+              name="location"
+              placeholder="Location"
+              value={editEvent ? editEvent.location : location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+            <div className="btns">
+              {editEvent ? (
+                <button type="submit">
+                  <IoAddCircleOutline /> Save Changes
+                </button>
+              ) : (
+                <button type="submit">
+                  <IoAddCircleOutline /> Add Event
+                </button>
+              )}
+              <button type="button" onClick={handleDownload}>
+                <FiDownload /> Download ics
+              </button>
             </div>
-          </div>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            placeholder="Event Description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <input
-            type="text"
-            id="location"
-            name="location"
-            placeholder="Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <div className="btns">
-            <button type="submit">
-              <IoAddCircleOutline /> Add Event
-            </button>
-            <button type="button" onClick={handleDownload}>
-              <FiDownload /> Download ics
-            </button>
-          </div>
-          <button onClick={() => setShowEvents(!showEvents)}>
-            {showEvents ? "Hide Added events" : "View all Added events"}
-          </button>
+          </form>
+        </div>
+        <button onClick={() => setShowEvents(!showEvents)}>
+          {showEvents ? "Hide Events" : "View all Events"}
+        </button>
 
-          {showEvents && (
-            <div>
-              {events < 1 ? <h3> No events added</h3> : <h3>Events</h3>}
-              {/* <h2>Events</h2> */}
-              <ul>
-                {events.map((event, index) => (
-                  <li key={index}>
-                    {event.title} - {event.start.join("/")} to{" "}
-                    {event.end.join("/")}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </form>
+        {showEvents && (
+          <div className="added-events">
+            {events < 1 ? (
+              <h4> No events added</h4>
+            ) : (
+              <Table striped>
+                <thead>
+                  <tr>
+                    <th>Event Title </th>
+                    <th>Event Date</th>
+                    <th>Start Time</th>
+                    {/* <th>Location</th> */}
+                    <th>Edit</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {events.map((event, index) => (
+                    <tr key={index}>
+                      <td> {event.title}</td>
+                      <td>{formatDate(event.start)}</td>
+                      <td>{formatTime(event.start)}</td>
+                      {/* <td>{event.location}</td> */}
+                      <td>
+                        <Dropdown>
+                          <Dropdown.Toggle
+                            className="toggle-btn"
+                            id="dropdown-basic"
+                            variant="outline-none"
+                            size="sm"
+                          >
+                            <IoSettingsOutline color="#000" />
+                          </Dropdown.Toggle>
+
+                          <Dropdown.Menu>
+                            <Dropdown.Item
+                              onClick={() => {
+                                setEditEvent(event);
+
+                                handleEditClick();
+                              }}
+                            >
+                              Edit
+                            </Dropdown.Item>
+
+                            <Dropdown.Item href="#" onClick={deleteClick}>
+                              Delete
+                            </Dropdown.Item>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                        <Overlay
+                          show={showPop}
+                          target={target}
+                          placement="top"
+                          container={popRef}
+                          containerPadding={20}
+                        >
+                          <Popover id="popover-contained">
+                            <Popover.Header as="h3">
+                              Are you sure you want to delete this event?
+                            </Popover.Header>
+                            <Popover.Body>
+                              <p>No go back</p>
+                              <p>Yes delete</p>
+                            </Popover.Body>
+                          </Popover>
+                        </Overlay>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            )}
+          </div>
+        )}
       </Modal.Body>
       <Modal.Footer>
         {/* <Button onClick={props.onHide}>Close</Button>
          */}
-        {/* {events.map((event, index) => (
-          <div key={index}>
-            <h3>{event.title}</h3>
-            <p>Start: {event.start.toString()}</p>
-            <p>End: {event.end.toString()}</p>
-            <p>Description: {event.description}</p>
-            <p>Location: {event.location}</p>
-            <hr />
-          </div>
-        ))} */}
       </Modal.Footer>
     </Modal>
   );
